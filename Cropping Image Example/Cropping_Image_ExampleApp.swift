b10bf0af357c4a137c2a548205a719862c95096e
Adrien <62828317+adrien1020@users.noreply.github.com>
2022-07-16 19:40:54 +0200

    Initial Commit

---
+++ Cropping Image Example/Cropping_Image_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cropping_Image_ExampleApp.swift
+//  Cropping Image Example
+//
+//  Created by Adrien Surugue on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cropping_Image_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
